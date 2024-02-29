91ede1d01daea13832fcf9088ee01739837f8163
Matteo Ballotta <matteo.ballotta51@gmail.com>
2024-02-29 11:08:53 +0100

    Aggiunta classe richieste GET HTTP

---
+++ MeteoFacile/Classes/HttpRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MeteoFacile.Classes
+{
+    // https://learn.microsoft.com/it-it/dotnet/api/system.net.http.httpclient?view=net-8.0
+    internal class HttpRequest
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public static async Task<(HttpStatusCode StatusCode, string Response)> Get(string url)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                return (response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+            catch (HttpRequestException ex)
+            {
+                return (HttpStatusCode.NotFound, ex.Message);
+            }
+
+        }
+    }
+}
