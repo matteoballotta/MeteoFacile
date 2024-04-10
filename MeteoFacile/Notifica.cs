a8ba3b3f7ad9634231d2e7716cf5fc371f6ebf46
Marica <156810156+Marica05@users.noreply.github.com>
2024-04-10 22:18:11 +0200

    Update Notifica.cs

--- MeteoFacile/Notifica.cs
+++ MeteoFacile/Notifica.cs
@@ -1,19 +1,19 @@
 using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
-
-private void ShowToastNotification ( string title, string content )
-{
-	ToastContent content = new ToastContent () {
-		Scenario = ToastScenario.Default,
-		Visual = new ToastVisual () {
-			BindingGeneric = new ToastBindingGeneric()
-			{
-				Children = {
-					new AdaptiveText () { Text = title }, 
-					new AdaptiveText () { Text = content }
+namespace MeteoFacile {
+	private void ShowToastNotification ( string title, string content ) {
+		ToastContent content = new ToastContent () {
+			Scenario = ToastScenario.Default,
+			Visual = new ToastVisual () {
+				BindingGeneric = new ToastBindingGeneric()
+				{
+					Children = {
+						new AdaptiveText () { Text = title }, 
+						new AdaptiveText () { Text = content }
+					}
 				}
 			}
-		}
-	};
-
-	ToastNotificationManagerCompat.CreateToastNotifier().Show(new ToastNotification(content.GetXml()));
+		};
+	
+		ToastNotificationManagerCompat.CreateToastNotifier().Show(new ToastNotification(content.GetXml()));
+	}
 }
