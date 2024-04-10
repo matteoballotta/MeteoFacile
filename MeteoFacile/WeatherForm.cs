60277ca37dcfb25927c21a82f7a89a9527315848
Marica <156810156+Marica05@users.noreply.github.com>
2024-04-10 22:02:20 +0200

    Update WeatherForm.cs
    Aggiunta di commenti

--- MeteoFacile/WeatherForm.cs
+++ MeteoFacile/WeatherForm.cs
@@ -70,10 +70,15 @@ namespace MeteoFacile
 
         private void CheckWeather (var latitudine, var longitudine) {
             if (weatherData != null && weatherData.Hourly != null) {
-                if (weatherData.Hourly.Temperature2m.Any (temperature => temperature > 40 || temperature < -10)) { ShowToastNotification ("Evento Atmosferico Estremo", "Temperature estreme segnalate!"); }
+                // Avvertenze su giornate superiori a 40° o inferiori a 0°
+                if (weatherData.Hourly.Temperature2m.Any (temperature => temperature > 40 || temperature < 0)) { ShowToastNotification ("Evento Atmosferico Estremo", "Temperature estreme segnalate!"); }
+                // Avvertenze su giornate piovose come pioggie / alluvioni
                 else if (weatherData.Hourly.Rain.Any (rain => rain > 20) ) { ShowToastNotification ("Evento Atmosferico Estremo", "Forte pioggia segnalata!"); }
+                // Avvertenze su giornate piovose estreme come temporali
                 else if (weatherData.Hourly.PrecipitationIntensity.Any(precipitation => precipitation > 0.7)) { ShowToastNotification("Evento Atmosferico Estremo", "Forte temporale in arrivo!"); }
+                // Avvertenze su giornate nevose
                 else if (weatherData.Hourly.SnowfallDepth.Any(snowfall => snowfall > 10)) { ShowToastNotification("Evento Atmosferico Estremo", "Nevicata intensa segnalata!"); }
+                // Avvertenze su giornate ventose (con forte vento)
                 else if (weatherData.Hourly.WindSpeed.Any(windSpeed => windSpeed > 30)) { ShowToastNotification("Evento Atmosferico Estremo", "Forte vento in arrivo!"); }
             }
         }
