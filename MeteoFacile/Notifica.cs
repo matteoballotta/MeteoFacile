911c38c695960d2371aa1dbfd2c79b258e265656
Marica <156810156+Marica05@users.noreply.github.com>
2024-04-11 08:35:48 +0200

    Modifica di notifica per inviare inviare un avviso popup

--- MeteoFacile/Notifica.cs
+++ MeteoFacile/Notifica.cs
@@ -1,19 +1,12 @@
-using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Uwp.Notifications
 namespace MeteoFacile {
 	private void ShowToastNotification ( string title, string content ) {
-		ToastContent content = new ToastContent () {
-			Scenario = ToastScenario.Default,
-			Visual = new ToastVisual () {
-				BindingGeneric = new ToastBindingGeneric()
-				{
-					Children = {
-						new AdaptiveText () { Text = title }, 
-						new AdaptiveText () { Text = content }
-					}
-				}
-			}
-		};
+		// Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+		new ToastContentBuilder()
+		    .AddArgument("action", "viewConversation")
+		    .AddText(title)
+		    .AddText(content)
+		    .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
 	
-		ToastNotificationManagerCompat.CreateToastNotifier().Show(new ToastNotification(content.GetXml()));
 	}
 }
