ce46dfe8c92a3c5008ce773392ddf4e41874fe6b
Marica <156810156+Marica05@users.noreply.github.com>
2024-04-17 22:44:15 +0200

    Aggiunta del limite di durata della notifica e della possibilità di inserire un immagine

--- MeteoFacile/Classes/NotificationManager.cs
+++ MeteoFacile/Classes/NotificationManager.cs
@@ -1,16 +1,50 @@
 using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.Storage;
 
-namespace MeteoFacile
-{
+namespace MeteoFacile {
     public class NotificationManager
-    {
+    {                          
+        private const string ImageUri = "":
+
         public static void ShowToastNotification(string title, string content)
+        {
+            var notice = new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddText(title)
+                .AddText(content);
+
+                if (!string.isNullOrEmpty(Image))
+                {
+                    notice.AddHeroImage(Image);
+                }
+
+                notice.Show (toast =>
+                {
+                    toast.ExpirationTime = DateTime.Now.AddDays (1);
+                });
+        }
+
+        public static void ShowToastNotification(string title, string content, int expiry)
         {
             new ToastContentBuilder()
                 .AddArgument("action", "viewConversation")
                 .AddText(title)
                 .AddText(content)
-                .Show();
+                .Show(toast =>
+                {
+                    toast.ExpirationTime = DateTime.Now.AddDays(expiry);
+                });
+        }
+
+        public static string Image {
+            get {
+                // Legge il percorso dell'immagine
+                return ApplicationData.Current.LocalSettings.Values[ImageUri] as string;
+            }
+
+            set {
+                ApplicationData.Current.LocalSettings.Values[ImageUri] = value;
+            }
         }
     }
 }
