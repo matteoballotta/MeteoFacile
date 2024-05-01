57232d92911148157caec965ff5300d4f61a5d19
Marica <156810156+Marica05@users.noreply.github.com>
2024-05-01 23:08:09 +0200

    Notifiche persoanlizzate

--- MeteoFacile/Classes/NotificationManager.cs
+++ MeteoFacile/Classes/NotificationManager.cs
@@ -2,30 +2,105 @@ using Microsoft.Toolkit.Uwp.Notifications;
 using System;
 using Windows.Storage;
 
-namespace MeteoFacile {
+namespace MeteoFacile
+{
     public class NotificationManager
     {
+        var ImageUri = null, header, text, button, header, emoji, bar, schedule;
+        var notification = new ToastContentBuilder();
+
         public static void ShowToastNotification(string title, string content, string imageUri = null)
         {
-            var notification = new ToastContentBuilder()
-                .AddArgument("action", "viewConversation")
+            notification.AddArgument("action", "viewConversation")
                 .AddText(title)
                 .AddText(content);
 
-                if (!string.IsNullOrEmpty(imageUri))
+            if (!string.IsNullOrEmpty(imageUri))
+            {
+                try
                 {
-                    try
-                    {
-                        notification.AddHeroImage(new System.Uri(imageUri));
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Immagine non trovata!");
-                    }
-                    
+                    notification.AddHeroImage(new System.Uri(imageUri));
+                }
+                catch
+                {
+                    Console.WriteLine("Immagine non trovata!");
                 }
 
-                notification.Show();
+            }
+
+            notification.Show();
         }
+
+        public static Boolean addImage = false;
+        public static Boolean addText = true;
+        //public static void addAudio (Boolean v);
+        public static Boolean addButton = false;
+        public static Boolean addHeader = true;
+        // public static Boolean addEmoji = false;
+        public static Boolean addProgressBar = false;
+        public static Boolean addSchedule = false;
+        // public static void addInputTextBox (Boolean v);
+
+        public static string Image {
+            get { return ImageUri; }
+            set { ImageUri = value; }
+        }
+
+        public static string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public static string Schedule
+        {
+            get { return schedule; }
+            set { schedule = value; }
+        }
+
+        public static string Bar
+        {
+            get { return bar; }
+            set { bar = value; }
+        }
+        
+        public static string Header
+        {
+            get { return header; }
+            set { header = value; }
+        }
+        
+        public static string Button
+        {
+            get { return bar; }
+            set { button = value; }
+        }
+
+        public static void ShowToastNotification()
+        {
+            
+            if (addButton == true)
+            {
+                notification.AddButton(Button);
+            }
+            if (addImage == true)
+            {
+                notification.AddHeroImage(Image);
+            }
+            if (addHeader == true)
+            {
+                notification.AddHeader(Header);
+            } 
+            if (addText == true)
+            {
+                notification.addText(Text);
+            }
+            if (addSchedule == true)
+            {
+                notification.AddSchedule(Schedule);
+            }
+
+            notification.Show();
+        }
+
     }
-}
