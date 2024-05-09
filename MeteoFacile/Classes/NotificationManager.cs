1814ca31485adf615d341fe3d2ab6e02e6da60e5
Marica <156810156+Marica05@users.noreply.github.com>
2024-05-09 06:05:42 +0200

    Aggiunta di personalizzazione

--- MeteoFacile/Classes/NotificationManager.cs
+++ MeteoFacile/Classes/NotificationManager.cs
@@ -5,6 +5,11 @@ using Windows.Storage;
 namespace MeteoFacile {
     public class NotificationManager
     {
+
+        boolean button = false;
+        boolean schedule = false;
+        boolean progresiveBar = false;
+    
         public static void ShowToastNotification(string title, string content, string imageUri = null)
         {
             var notification = new ToastContentBuilder()
@@ -12,20 +17,44 @@ namespace MeteoFacile {
                 .AddText(title)
                 .AddText(content);
 
-                if (!string.IsNullOrEmpty(imageUri))
+            if (!string.IsNullOrEmpty(imageUri))
+            {
+                try
                 {
-                    try
-                    {
-                        notification.AddHeroImage(new System.Uri(imageUri));
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Immagine non trovata!");
-                    }
-                    
+                    notification.AddHeroImage(new System.Uri(imageUri));
                 }
+                catch
+                {
+                    Console.WriteLine("Immagine non trovata!");
+                }
+                
+            }
 
-                notification.Show();
+            if (button == true) 
+            {
+                builder.AddButton (new ToastButtonDismiss());
+            }
+
+            if (progressiveBar == true)
+            {
+                builder.AddProgressBar (new ToastProgressBar()
+                {
+                    Title = "Tempo rimanente",
+                    Value = 0,
+                    ValueStringOverride = "0%",
+                    Indeterminate = false
+                });
+            }
+            
+            if (schedule == true)
+            {
+                builder.AddScheduledToast(new ScheduledToastNotification(
+                    builder.GetToastContent().GetXml(),
+                    DateTime.Now.AddSeconds(30)));
+
+            }
+
+            notification.Show();
         }
     }
 }
